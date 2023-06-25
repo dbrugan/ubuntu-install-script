8c36f1847b47f6f1e143519669dfcece28afa4a8
Bruno Dias <brunophm@gmail.com>
2023-06-24 23:49:03 -0300

    :sparkles: Feat: initial structure
    finished the initial structure of the code that the script will use

--- install.ts
+++ install.ts
@@ -1,1 +1,15 @@
- 
+// step 1: partitioning
+//
+// step 2: format partitions
+//
+// step 3: mount partitions
+//
+// step 4: install base system
+//
+// step 5: configure system
+//
+// step 6: install bootloader
+//
+// step 7: post-install tasks
+
+
