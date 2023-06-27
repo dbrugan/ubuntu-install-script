8a3720250595253211249638514534dc70a45f2d
Bruno Dias <brunophm@gmail.com>
2023-06-27 00:06:09 -0300

    :sparkles: Feat: mount btrfs subvolumes

--- install.py
+++ install.py
@@ -11,14 +11,28 @@ subprocess.run(["parted", "/dev/sda", "set", "1", "boot", "on"])
   # encrypt partitions
 subprocess.run(["cryptsetup", "luksFormat", "/dev/sda2"])
 subprocess.run(["cryptsetup", "open", "--type", "luks", "/dev/sda2", "sda2_crypt"])
-subprocess.run(["mkfs.ext4", "/dev/mapper/sda2_crypt"]) 
+subprocess.run(["mkfs.btrfs", "/dev/mapper/sda2_crypt"]) 
 
   # mount partitions
+subprocess.run(["mount", "/dev/mapper/sda2_crypt", "/mnt"])
+
+ # creating btrfs subvolumes
+subprocess.run(["btrfs", "subvolume", "create", "/mnt/root"])
+subprocess.run(["btrfs", "subvolume", "create", "/mnt/home"])
+subprocess.run(["btrfs", "subvolume", "create", "/mnt/snapshots"])
+subprocess.run(["umount", "/mnt"])
+
+ # mount subvolumes
+subprocess.run(["mount", "-o", "subvol=root", "/dev/mapper/sda2_crypt", "/mnt"])
+subprocess.run(["mkdir", "/mnt/home"])
+subprocess.run(["mount", "-o", "subvol=home", "/dev/mapper/sda2_crypt", "/mnt/home"])
+subprocess.run(["mkdir", "/mnt/.snapshots"])
+subprocess.run(["mount", "-o", "subvol=snapshots", "/dev/mapper/sda2_crypt", "/mnt/.snapshots"])
+
+  # install base system
   #
-  # step 4: install base system
+  # configure system
   #
-  # step 5: configure system
+  # install bootloader
   #
-  # step 6: install bootloader
-  #
-  # step 7: post-install tasks
+  # post-install tasks
